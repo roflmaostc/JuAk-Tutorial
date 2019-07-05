9bd4215aae269ec2d27669456dc6f6c9e1987b4d
ga63ruh <maximilian.schaeffeler@tum.de>
2019-07-05 23:37:39 +0200

    Update tutorial

--- gloss-exercises/src/Main.hs
+++ gloss-exercises/src/Main.hs
@@ -3,61 +3,69 @@ module Main where
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
 
-
 data WorldTyp = World { 
-    _xPos     :: Int,
-    _yPos     :: Int,
-    _breite   :: Int,
-    _lPressed :: Bool,
-    _rPressed :: Bool
+    xPos     :: Float,
+    yPos     :: Float,
+    breite   :: Float,
+    rPressed :: Bool,
+    lPressed :: Bool
   } deriving (Eq, Show)
 
 
 initialWorld :: WorldTyp
 initialWorld = World { 
-    _xPos     = 0,
-    _yPos     = 0,
-    _breite   = 100,
-    _lPressed = False,
-    _rPressed = False
+    xPos     = 0,
+    yPos     = 0,
+    breite   = 100,
+    rPressed = False,
+    lPressed = False
   }
 
 
 draw :: WorldTyp -> Picture
 draw world = ball
     where
-        xPos = fromIntegral $ _xPos world
-        yPos = fromIntegral $ _yPos world
-        ball = Color red $ translate xPos yPos $ circleSolid 5
+        x = xPos world
+        y = yPos world
+        ball = color red $ translate x y $ circleSolid 5
 
 handleKeys :: Event -> WorldTyp -> WorldTyp
-handleKeys (EventKey (SpecialKey KeyLeft ) Down _ _ ) world = world {_lPressed = True}
-handleKeys (EventKey (SpecialKey KeyLeft ) Up _ _ ) world = world {_lPressed = False}
-handleKeys (EventKey (SpecialKey KeyRight) Down _ _ ) world = world {_rPressed = True}
-handleKeys (EventKey (SpecialKey KeyRight) Up _ _ ) world = world {_rPressed = False}
+handleKeys (EventKey (SpecialKey KeyLeft) Down _ _ ) world = 
+  world { lPressed = True }
+handleKeys (EventKey (SpecialKey KeyLeft) Up _ _ ) world = 
+  world { lPressed = False}
+handleKeys (EventKey (SpecialKey KeyRight) Down _ _ ) world = 
+  world { rPressed = True }
+handleKeys (EventKey (SpecialKey KeyRight) Up _ _ ) world =
+  world { rPressed = False }
 handleKeys _ world = world
 
 update :: Float -> WorldTyp -> WorldTyp
-update _ world = case (_lPressed world, _rPressed world) of
+update _ world = case (lPressed world, rPressed world) of
+    -- Taste nach links gedrückt
     (True, False) -> 
-        if xPos > - breite 
-        then world { _xPos = xPos - 2 } 
+        if x > - b
+        then world { xPos = x - 2 } 
         else world
+    -- Taste nach rechts gedrückt
     (False, True) -> 
-        if xPos < breite
-        then world { _xPos = xPos + 2 }
+        if x < b
+        then world { xPos = x + 2 }
         else world
     _ ->
         world
     where
-        xPos = _xPos world
-        breite = _breite world
+        x = xPos world
+        b = breite world
 
+-- Spielt im Vollbild, mit 25 Bildern pro Sekunde.
+-- Zum Zeichnen wird die Funktion "draw" benutzt.
+-- Wird eine Taste gedrückt oder losgelassen, wird die Funktion "handleKeys"
+-- aufgerufen.
+-- 25 mal pro Sekunde wird "update" aufgerufen.
+main = play FullScreen white 25 initialWorld draw handleKeys update
 
-main = do
-  play FullScreen white 25 initialWorld draw handleKeys update
-
--- Aufgaben
+-- == Aufgaben ==
 -- 1.
 -- Erweitere das Spiel so, dass der rote Punkt sich nur
 -- in einem Rechteck bewegen darf.
@@ -80,6 +88,7 @@ main = do
 -- Baue daraus einen Wettkampf.
 -- Spieler 1 muss Spieler 2 fangen. Schafft er dies, so soll dies auf dem
 -- Bildschirm in irgendeiner Art und Weise gekennzeichnet werden.
+
 
 -- 5.
 -- Erweitere das Spiel um einen weiteren Gegner.
