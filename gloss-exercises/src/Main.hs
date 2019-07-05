6a66c9912ecb429d532be953508af233bd50ab1b
ga63ruh <maximilian.schaeffeler@tum.de>
2019-07-05 15:59:40 +0200

    Fix type

--- gloss-exercises/src/Main.hs
+++ gloss-exercises/src/Main.hs
@@ -1,60 +1,61 @@
-
-
 module Main where
 
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
 
 
-data World = World
-	{ _xPos     :: Int
-	, _yPos     :: Int
-	, _breite   :: Int
-	, _lPressed :: Bool
-	, _rPressed :: Bool
-	}
+data WorldTyp = World { 
+    _xPos     :: Int,
+    _yPos     :: Int,
+    _breite   :: Int,
+    _lPressed :: Bool,
+    _rPressed :: Bool
+  } deriving (Eq, Show)
 
 
-initialWorld :: World
-initialWorld = World
-	{ _xPos     = 0
-	, _yPos     = 0
-	, _breite   = 100
-	, _lPressed = False
-	, _rPressed = False
-	}
+initialWorld :: WorldTyp
+initialWorld = World { 
+    _xPos     = 0,
+    _yPos     = 0,
+    _breite   = 100,
+    _lPressed = False,
+    _rPressed = False
+  }
 
 
-draw :: World -> Picture
+draw :: WorldTyp -> Picture
 draw world = ball
-	where
-		xPos = fromIntegral $ _xPos world
-		yPos = fromIntegral $ _yPos world
-		ball = Color red $ translate xPos yPos $ circleSolid 5
+    where
+        xPos = fromIntegral $ _xPos world
+        yPos = fromIntegral $ _yPos world
+        ball = Color red $ translate xPos yPos $ circleSolid 5
 
-
-handleKeys :: Event -> World -> World
+handleKeys :: Event -> WorldTyp -> WorldTyp
 handleKeys (EventKey (SpecialKey KeyLeft ) Down _ _ ) world = world {_lPressed = True}
 handleKeys (EventKey (SpecialKey KeyLeft ) Up _ _ ) world = world {_lPressed = False}
 handleKeys (EventKey (SpecialKey KeyRight) Down _ _ ) world = world {_rPressed = True}
 handleKeys (EventKey (SpecialKey KeyRight) Up _ _ ) world = world {_rPressed = False}
 handleKeys _ world = world
 
-
-update :: Float -> World -> World
+update :: Float -> WorldTyp -> WorldTyp
 update _ world = case (_lPressed world, _rPressed world) of
-		(True, False) -> if xPos > - breite then world { _xPos = xPos -2 } else world
-		(False, True) -> if xPos < breite then world { _xPos = xPos +2 } else world
-		_ -> world
-	where
-		xPos = _xPos world
-		breite = _breite world
+    (True, False) -> 
+        if xPos > - breite 
+        then world { _xPos = xPos - 2 } 
+        else world
+    (False, True) -> 
+        if xPos < breite
+        then world { _xPos = xPos + 2 }
+        else world
+    _ ->
+        world
+    where
+        xPos = _xPos world
+        breite = _breite world
 
 
 main = do
   play FullScreen white 25 initialWorld draw handleKeys update
-
-
 
 -- Aufgaben
 -- 1.
@@ -79,7 +80,6 @@ main = do
 -- Baue daraus einen Wettkampf.
 -- Spieler 1 muss Spieler 2 fangen. Schafft er dies, so soll dies auf dem
 -- Bildschirm in irgendeiner Art und Weise gekennzeichnet werden.
-
 
 -- 5.
 -- Erweitere das Spiel um einen weiteren Gegner.
